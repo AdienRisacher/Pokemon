6ba42934069ae9d2eee2deb31746141317b3ab87
Aurélie <Aurlie>
2023-07-03 11:27:13 +0200

    add controllers and forms

---
+++ src/Controller/ElementaryTypeController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ElementaryType;
+use App\Form\ElementaryTypeType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/elementary/type')]
+class ElementaryTypeController extends AbstractController
+{
+    #[Route('/', name: 'app_elementary_type_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $elementaryTypes = $entityManager
+            ->getRepository(ElementaryType::class)
+            ->findAll();
+
+        return $this->render('elementary_type/index.html.twig', [
+            'elementary_types' => $elementaryTypes,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_elementary_type_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $elementaryType = new ElementaryType();
+        $form = $this->createForm(ElementaryTypeType::class, $elementaryType);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($elementaryType);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_elementary_type_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('elementary_type/new.html.twig', [
+            'elementary_type' => $elementaryType,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_elementary_type_show', methods: ['GET'])]
+    public function show(ElementaryType $elementaryType): Response
+    {
+        return $this->render('elementary_type/show.html.twig', [
+            'elementary_type' => $elementaryType,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_elementary_type_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, ElementaryType $elementaryType, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ElementaryTypeType::class, $elementaryType);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_elementary_type_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('elementary_type/edit.html.twig', [
+            'elementary_type' => $elementaryType,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_elementary_type_delete', methods: ['POST'])]
+    public function delete(Request $request, ElementaryType $elementaryType, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$elementaryType->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($elementaryType);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_elementary_type_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Controller/PokemonTypeController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PokemonType;
+use App\Form\PokemonTypeType;
+use App\Repository\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/pokemon/type')]
+class PokemonTypeController extends AbstractController
+{
+    #[Route('/', name: 'pokemon_type_index', methods: ['GET'])]
+    public function index(EntityRepository $entityRepository): Response
+    {
+        return $this->render('pokemon_type/index.html.twig', [
+            'pokemon_types' => $entityRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'pokemon_type_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $pokemonType = new PokemonType();
+        $form = $this->createForm(PokemonTypeType::class, $pokemonType);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($pokemonType);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('pokemon_type_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('pokemon_type/new.html.twig', [
+            'pokemon_type' => $pokemonType,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'pokemon_type_show', methods: ['GET'])]
+    public function show(PokemonType $pokemonType): Response
+    {
+        return $this->render('pokemon_type/show.html.twig', [
+            'pokemon_type' => $pokemonType,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'pokemon_type_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, PokemonType $pokemonType, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PokemonTypeType::class, $pokemonType);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('pokemon_type_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('pokemon_type/edit.html.twig', [
+            'pokemon_type' => $pokemonType,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'pokemon_type_delete', methods: ['POST'])]
+    public function delete(Request $request, PokemonType $pokemonType, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$pokemonType->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($pokemonType);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('pokemon_type_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
